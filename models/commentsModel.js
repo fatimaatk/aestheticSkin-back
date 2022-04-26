ea3672ad31472e519b674d20a08c7600d003dd96
Fatima Ait Khelifa <faitkhelifa@DOMOTI.FR>
2022-04-26 11:34:52 +0200

    back

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -2,7 +2,7 @@ import dbConnect from '../config/db-config.js';
 
 const getAllComments = (id) => {
     return new Promise((resolve, reject) => {
-        dbConnect.query('SELECT rate_comment.rate_id, rate_comment.comment, rate_comment.product_id, products.title, user.email FROM rate_comment INNER JOIN products ON rate_comment.product_id = products.id INNER JOIN user ON rate_comment.user_id = user.id ORDER BY product_id', id, (err, results) => {
+        dbConnect.query('SELECT rate_comment.rate_id, rate_comment.comment, rate_comment.product_id, products.title, products.image1, user.email, rate_comment.isVisible FROM rate_comment INNER JOIN products ON rate_comment.product_id = products.id INNER JOIN user ON rate_comment.user_id = user.id', id, (err, results) => {
             if (err) reject(err);
             else resolve(results);
         })
