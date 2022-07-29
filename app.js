76cd03478edc3f95bdbab04703aba59b38226d13
Faitkhelifa <Faitkhelifa@DMINFOX021>
2022-07-29 16:56:30 +0200

    wip

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { Server } from "socket.io";
 
 
 // Import du fichier de gestion des routes || ROUTER DU SERVEUR
 import { setupRoutes } from './routes/router.js';
-
 
 const app = express();
 const port = 8000;
@@ -22,6 +22,20 @@ app.use(express.urlencoded({ extended: true })); // parse les request.body en ur
 setupRoutes(app);
 
 // Serveur is running
-app.listen(port, () => console.log('Server is running on port ' + port));
+const server = app.listen(port, () => console.log('Server is running on port ' + port));
 
-export default app;
+// Initialisation du socket.io avec liasion de notre serveur back vers front
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  },
+});
+
+//Récupération des messages
+io.on('connection', (socket) => {
+  console.log(`Un utilisateur est connecté : ${socket.id}`)
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data)
+  })
+})
